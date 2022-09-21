0c9d2db197b30d2074aeb23c4dd45cd3609d09e6
Anton <anton.luning@xperdi.com>
2022-09-21 10:49:50 +0200

    Updated premake version and location
    - Also buildscripts premake call

--- BuildScripts/GenerateVS2019.bat
+++ BuildScripts/GenerateVS2019.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0\..\
-call Vendor\premake\bin\premake5.exe vs2019
+call Vendor\premake\premake5.exe vs2019
 popd
 PAUSE
--- BuildScripts/GenerateVS2022.bat
+++ BuildScripts/GenerateVS2022.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0\..\
-call Vendor\premake\bin\premake5.exe vs2022
+call Vendor\premake\premake5.exe vs2022
 popd
 PAUSE
